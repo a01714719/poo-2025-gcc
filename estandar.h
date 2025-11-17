5e90cea076b713ac4e5e3e8ebba2c9b31ac2942a
a01714719 <a01714719@tec.mx>
2025-11-16 23:33:30 -0600

    Update estandar.h

--- estandar.h
+++ estandar.h
@@ -1,1 +1,24 @@
+#ifndef ESTANDAR_H
+#define ESTANDAR_H
+#include <string>
+using namespace std;
 
+class Estandar {
+private:
+    string cliente;
+    int meses;
+    double precioMensual;
+
+public:
+    Estandar(string c, int m) : cliente(c), meses(m), precioMensual(500) {}
+
+    double calcularPago() {
+        return meses * precioMensual;
+    }
+
+    string info() {
+        return "Membresia Estandar para " + cliente;
+    }
+};
+
+#endif
