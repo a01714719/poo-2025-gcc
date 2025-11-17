8427a0c65afffd4d6de4fd23267f94620160e9eb
a01714719 <a01714719@tec.mx>
2025-11-16 23:35:15 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include "premium.h"
+#include "estandar.h"
+#include "cortesia.h"
+using namespace std;
+
+int main() {
+    Premium p("Sebastian", 3);
+    Estandar e("Erik", 2);
+    Cortesia c("Invitado");
+
+    cout << p.info() << " | Pago: $" << p.calcularPago() << endl;
+    cout << e.info() << " | Pago: $" << e.calcularPago() << endl;
+    cout << c.info() << " | Pago: $" << c.calcularPago() << endl;
+
+    return 0;
+}
 
