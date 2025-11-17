55949f19f5644fc9743d7a419f88d474459ff3fe
a01714719 <a01714719@tec.mx>
2025-11-16 23:34:44 -0600

    Update cortesia.h

--- cortesia.h
+++ cortesia.h
@@ -1,1 +1,23 @@
+#ifndef CORTESIA_H
+#define CORTESIA_H
+#include <string>
+using namespace std;
+
+class Cortesia {
+private:
+    string cliente;
+
+public:
+    Cortesia(string c) : cliente(c) {}
+
+    double calcularPago() {
+        return 0;
+    }
+
+    string info() {
+        return "Membresia de Cortesia para " + cliente;
+    }
+};
+
+#endif
 
