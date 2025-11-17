7c8501969e0a106e5edd7b3b716883b8c28802ae
a01714719 <a01714719@tec.mx>
2025-11-16 23:34:12 -0600

    Update premium.h

--- premium.h
+++ premium.h
@@ -1,1 +1,25 @@
+#ifndef PREMIUM_H
+#define PREMIUM_H
+#include <string>
+using namespace std;
+
+class Premium {
+private:
+    string cliente;
+    int meses;
+    double precioMensual;
+
+public:
+    Premium(string c, int m) : cliente(c), meses(m), precioMensual(1000) {}
+
+    double calcularPago() {
+        return meses * precioMensual;
+    }
+
+    string info() {
+        return "Membresia Premium para " + cliente;
+    }
+};
+
+#endif
 
