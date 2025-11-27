590fc2261e42fa8fa5f257b5ebc2bf7a621960a1
a01714719 <a01714719@tec.mx>
2025-11-26 21:48:52 -0600

    Update estandar.h

--- estandar.h
+++ estandar.h
@@ -10,14 +10,25 @@ private:
     double precioMensual;
 
 public:
-    Estandar(string c, int m) : cliente(c), meses(m), precioMensual(500) {}
+    Estandar(string c = "", int m = 0) : cliente(c), meses(m), precioMensual(500) {}
+
+    // GETTERS
+    string getCliente() const { return cliente; }
+    int getMeses() const { return meses; }
+    double getPrecioMensual() const { return precioMensual; }
+
+    // SETTERS
+    void setCliente(const string &c) { cliente = c; }
+    void setMeses(int m) { meses = m; }
+    void setPrecioMensual(double p) { precioMensual = p; }
 
     double calcularPago() {
         return meses * precioMensual;
     }
 
     string info() {
-        return "Membresia Estandar para " + cliente;
+        return "Membresia Estandar para " + cliente +
+               " por " + to_string(meses) + " meses";
     }
 };
 
