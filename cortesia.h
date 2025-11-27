eb471a4660e8e436261667d47cead6af23348e3f
a01714719 <a01714719@tec.mx>
2025-11-26 21:49:10 -0600

    Update cortesia.h

--- cortesia.h
+++ cortesia.h
@@ -6,18 +6,27 @@ using namespace std;
 class Cortesia {
 private:
     string cliente;
+    int meses;
 
 public:
-    Cortesia(string c) : cliente(c) {}
+    Cortesia(string c = "", int m = 0) : cliente(c), meses(m) {}
+
+    // GETTERS
+    string getCliente() const { return cliente; }
+    int getMeses() const { return meses; }
+
+    // SETTERS
+    void setCliente(const string &c) { cliente = c; }
+    void setMeses(int m) { meses = m; }
 
     double calcularPago() {
         return 0;
     }
 
     string info() {
-        return "Membresia de Cortesia para " + cliente;
+        return "Membresia de Cortesia para " + cliente +
+               " por " + to_string(meses) + " meses";
     }
 };
 
 #endif
-
