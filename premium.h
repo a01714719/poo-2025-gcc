91e6cff6fc2b4813cad64e7687541bdeff061855
a01714719 <a01714719@tec.mx>
2025-11-26 21:48:28 -0600

    Update premium.h

--- premium.h
+++ premium.h
@@ -10,16 +10,26 @@ private:
     double precioMensual;
 
 public:
-    Premium(string c, int m) : cliente(c), meses(m), precioMensual(1000) {}
+    Premium(string c = "", int m = 0) : cliente(c), meses(m), precioMensual(1000) {}
+
+    // GETTERS
+    string getCliente() const { return cliente; }
+    int getMeses() const { return meses; }
+    double getPrecioMensual() const { return precioMensual; }
+
+    // SETTERS
+    void setCliente(const string &c) { cliente = c; }
+    void setMeses(int m) { meses = m; }
+    void setPrecioMensual(double p) { precioMensual = p; }
 
     double calcularPago() {
         return meses * precioMensual;
     }
 
     string info() {
-        return "Membresia Premium para " + cliente;
+        return "Membresia Premium para " + cliente + 
+               " por " + to_string(meses) + " meses";
     }
 };
 
 #endif
-
