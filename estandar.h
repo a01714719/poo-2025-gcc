f798bb905984500569e3fdafb3f3e8517acaf72b
a01714719 <a01714719@tec.mx>
2025-12-03 23:26:31 -0600

    Update estandar.h

--- estandar.h
+++ estandar.h
@@ -1,34 +1,26 @@
 #ifndef ESTANDAR_H
 #define ESTANDAR_H
+#include "membresia.h" 
 #include <string>
-using namespace std;
 
-class Estandar {
+class Estandar : public Membresia {
 private:
-    string cliente;
-    int meses;
-    double precioMensual;
+    double precioMensual; 
 
 public:
-    Estandar(string c = "", int m = 0) : cliente(c), meses(m), precioMensual(500) {}
+    Estandar(string c = "", int m = 0) : Membresia(c, m), precioMensual(500) {}
 
-    // GETTERS
-    string getCliente() const { return cliente; }
-    int getMeses() const { return meses; }
+    // GETTER/SETTER 
     double getPrecioMensual() const { return precioMensual; }
-
-    // SETTERS
-    void setCliente(const string &c) { cliente = c; }
-    void setMeses(int m) { meses = m; }
     void setPrecioMensual(double p) { precioMensual = p; }
-
-    double calcularPago() {
+    
+    double calcularPago() override {
         return meses * precioMensual;
     }
 
-    string info() {
+    string info() override {
         return "Membresia Estandar para " + cliente +
-               " por " + to_string(meses) + " meses";
+               " por " + to_string(meses) + " meses (Costo Mensual: $" + to_string(precioMensual) + ")";
     }
 };
 
