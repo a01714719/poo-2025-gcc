8b4cd5944ac0ff289aaff10a1f5336160baa1ded
a01714719 <a01714719@tec.mx>
2025-12-07 21:03:45 -0600

    Update Cortesia.h

--- Cortesia.h
+++ Cortesia.h
@@ -1,17 +1,25 @@
 #ifndef CORTESIA_H
 #define CORTESIA_H
-#include "membresia.h" 
+// Archivo: Cortesia.h
+// Descripción: Implementa la membresía Cortesía del gimnasio.
+// Relación UML: Hereda de la clase base abstracta Membresia.
 #include <string>
+#include "membresias.h"
 
+using namespace std;
+// Clase Cortesia
+// Representa una membresía gratuita
 class Cortesia : public Membresia {
-public:
-    
-    Cortesia(string c = "", int m = 0) : Membresia(c, m) {}
 
+    public:
+    // Constructores
+    Cortesia(string c = "", int m = 0) : Membresia(c, m) {}
+    // Métodos de Membresia
+    // Cortesía no genera pago.
     double calcularPago() override {
         return 0.0;
     }
-
+    // Devuelve una descripción de la membresía para el reporte.
     string info() override {
         return "Membresia de Cortesia (GRATIS) para " + cliente +
                " por " + to_string(meses) + " meses";
