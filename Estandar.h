bc08219eee4b84b479a40b45deae083417308743
a01714719 <a01714719@tec.mx>
2025-12-07 21:03:26 -0600

    Update Estandar.h

--- Estandar.h
+++ Estandar.h
@@ -1,26 +1,36 @@
 #ifndef ESTANDAR_H
 #define ESTANDAR_H
-#include "membresia.h" 
+// Archivo: Estandar.h
+// Descripción: Implementa la membresía Estándar del gimnasio.
+// Relación UML: Hereda de la clase base abstracta Membresia.
 #include <string>
+#include "membresias.h"
 
+using namespace std;
+// Clase Estandar
+// Representa una membresía con precio mensual fijo.
 class Estandar : public Membresia {
 private:
-    double precioMensual; 
+    // Atributo específico de Estándar
+    double precioMensual;
 
 public:
-    Estandar(string c = "", int m = 0) : Membresia(c, m), precioMensual(500) {}
-
-    // GETTER/SETTER 
+    // Constructores
+    // Define el precio mensual en 500 
+    Estandar(string c = "", int m = 0)
+        : Membresia(c, m), precioMensual(500) {}
+    // Getters y setters
     double getPrecioMensual() const { return precioMensual; }
     void setPrecioMensual(double p) { precioMensual = p; }
-    
+    // Métodos  de Membresia
     double calcularPago() override {
         return meses * precioMensual;
     }
-
+    // Devuelve una descripción de la membresía para el reporte.
     string info() override {
         return "Membresia Estandar para " + cliente +
-               " por " + to_string(meses) + " meses (Costo Mensual: $" + to_string(precioMensual) + ")";
+               " por " + to_string(meses) +
+               " meses (Costo Mensual: $" + to_string(precioMensual) + ")";
     }
 };
 
